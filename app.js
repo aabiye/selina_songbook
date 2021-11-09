c872c33455df15a0985ec258fc87fa8cf2f71a33
James <james.gordon1@verizon.com>
2021-11-09 16:25:21 -0600

    Added authorization funciton to test route on users

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 
-// const jwtCheck = jwt({
-//       secret: jwks.expressJwtSecret({
-//           cache: true,
-//           rateLimit: true,
-//           jwksRequestsPerMinute: 5,
-//           jwksUri: 'https://dev-26-ppi9m.us.auth0.com/.well-known/jwks.json'
-//     }),
-//     audience: 'selinaSongbook ',
-//     issuer: 'https://dev-26-ppi9m.us.auth0.com/',
-//     algorithms: ['RS256']
-// });
+const jwtCheck = jwt({
+      secret: jwks.expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: 'https://dev-26-ppi9m.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'selinaSongbook ',
+    issuer: 'https://dev-26-ppi9m.us.auth0.com/',
+    algorithms: ['RS256']
+});
 
-// app.use(jwtCheck);
+app.use(jwtCheck);
 
 app.get('/authorized', function (req, res) {
     res.send('Secured Resource');
